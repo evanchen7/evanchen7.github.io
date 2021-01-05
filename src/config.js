239f37af273406203c6f6a2aa4ed82e888717859
Evan <evanchen7@yahoo.com>
2021-01-05 03:12:15 -0800

    Save work

---
+++ src/config.js
@@ -0,0 +1,32 @@
+export const config = {
+  // Your Github Converted Token To Know How To Get Your Token Look at Readme.md
+  githubConvertedToken: "ZmNjYTk5NzQ2ZDllMThhZWQ0Y2Q4NzY0OTc0NjQ2NmI4N2JmOGNlNQ==",
+
+  // Your Github UserName
+  githubUserName: "evanchen7",
+
+  // Your Github Repo Name Where You Have your issues as Blog
+  githubRepo: "evanchen7.github.io",
+
+  // Set it to true if you have a Github app to add to this project
+  // and fill the client ID & secret
+  enableOAuth: true,
+  OAuthClientID: 'Iv1.338e1a4c2d1247e3',
+  OAuthSecret: 'e07e0551184227d2062f3196d9126a6fd2f05708',
+
+  // Your Personal Blog Title
+  title : "Evan Chen" ,
+
+  // Your Personal Blog Subtitle
+  subtitle : "Software Developer",
+
+  // Header customization
+  header: {
+    backgroundColor: '#f1f6f8', // can be a CSS gradient
+    backgroundColorDark: '#4C566A',
+    titleColor: '#ff5252',
+    titleColorDark: '#16a085',
+    subtitleColor: '#37474f',
+    subtitleColorDark: '#D8DEE9',
+  },
+};
